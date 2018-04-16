48e7c69e56a025523e4ac428da785f6829034bf3
shridatta <sgz2@njit.edu>
2018-04-16 16:31:44 -0400

    connecting to mongodb with mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.export = {
+  mongoURI:
+'mongodb://shridatta:root@ds247449.mlab.com:47449/social_network'
+};
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB configstore
+const db = require('./config/keys.js').mongoURI;
+
+//connect to mongodb
+
+mongoose
+    .connect('mongodb://shridatta:root@ds247449.mlab.com:47449/social_network')
+    .then(()=>console.log('MongoDB connected'))
+    .catch(err=>console.log(err));
+
 
 app.get('/',(req,res)=>res.send('Hello'));
 
